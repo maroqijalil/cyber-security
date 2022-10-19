2e00f9f3af03b24877d850b30deba956a1999638
maroqijalil <jalilpro31@gmail.com>
2022-10-19 19:50:32 +0700

    refactor: change output

--- assignment-1/separated/main.py
+++ assignment-1/separated/main.py
@@ -2,13 +2,13 @@ from linear_des import LinearDES
 
 
 if __name__ == "__main__":
-  plain_text = "1234adAB8912382ashdajsdkasjd9283989hjwo8u3oik1231"
-  key = "jalilasdasjdkasdjsakjdhaksjdh9832y948hewokjhk"
+    plain_text = "1234adAB891238klashkdakskjdk2"
+    key = "jalilas"
 
-  print("Encryption")
-  cipher_text = LinearDES(plain_text, key).encrypt()
-  print("Cipher Text : ", cipher_text)
+    print("Encryption")
+    cipher_text = LinearDES(plain_text, key).encrypt()
+    print("Cipher Text : ", cipher_text)
 
-  print("Decryption")
-  text = LinearDES(cipher_text, key).decrypt()
-  print("Plain Text : ", text)
+    print("Decryption")
+    text = LinearDES(cipher_text, key).decrypt()[:len(plain_text)]
+    print("Plain Text : ", text)
